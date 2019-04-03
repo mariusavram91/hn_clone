ae79c0e04aec554052c154d2a2a2804e2536a7e6
Marius Avram <me@mariusavram.com>
2019-04-03 02:45:31 +0200

    Add comments to api.js

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -28,6 +28,8 @@ const request = function(url) {
         });
 }
 
+/* Can return time difference between now and a give date time object in
+ * seconds, minutes, hours, or days. */
 const getTimeDifferenceToNow = {
     inSeconds: function(date) {
         const t1 = date.getTime();
@@ -51,6 +53,7 @@ const getTimeDifferenceToNow = {
     },
 }
 
+/* Returns presentable time since a given date time object */
 const getTimeToNow = function(date) {
     const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
@@ -94,6 +97,8 @@ const getTimeToNow = function(date) {
     return time + show;
 }
 
+/* Returns an object with the start item number, the end item number, and the
+ * next page value. */
 const paginate = function(currentPage) {
     let start = 0;
     let end = pagination;
@@ -112,12 +117,14 @@ const paginate = function(currentPage) {
     };
 }
 
+/* Removes all content from the tbody of the given table id */
 const cleanBodyOf = function(id) {
     const itemList = document.getElementById(id);
     const itemListBody = itemList.getElementsByTagName('tbody')[0];
     itemListBody.innerHTML = "";
 }
 
+/* Toggles the 'Loading...' text */
 const toggleLoading = function() {
     let loading = document.getElementById('loading');
     if(loading.style.display !== "none") {
@@ -127,12 +134,14 @@ const toggleLoading = function() {
     }
 }
 
+/* Will return the page number from the URL hash */
 const getPageFromUrl = function() {
     if(window.location.hash) {
         return parseInt(window.location.hash.substr(1));
     }
 }
 
+/* Fetches and populates the stories for a given page */
 const getStoriesForCurrentPage = function(page) {
     const pageFromUrl = getPageFromUrl();
     if(pageFromUrl && page === 1) {
@@ -144,6 +153,7 @@ const getStoriesForCurrentPage = function(page) {
     }
 }
 
+/* Checks if the current page is for a story or not */
 const isStoryView = function() {
     if(window.location.href.indexOf("story") > -1) {
         return true;
@@ -152,6 +162,7 @@ const isStoryView = function() {
     return false;
 }
 
+/* Checks if the current page is for the newest stories */
 const isNewStoriesView = function() {
     if(window.location.href.indexOf("new") > -1) {
         return true;
@@ -160,10 +171,12 @@ const isNewStoriesView = function() {
     return false;
 }
 
+/* Returns the story Id from the story view URL hash */
 const getStoryIdFromUrl = function() {
     return window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
 }
 
+/* Fetches stories ids and page items, then populates the data in the table */
 const getStories = function(page) {
     const p = paginate(page);
 
@@ -181,12 +194,14 @@ const getStories = function(page) {
             }));
         });
 
+        // Will get all the necessary stories together, sorted
         Promise.all(storiesPromises).then(function(storiesData) {
             populateStories(storiesData, p.start + 1, p.nextPage);
         });
     });
 }
 
+/* Fetches a story and its top level comments and populates the data */
 const getComments = function(storyId) {
     HackerNews.getItem(storyId).then(function(story) {
         if(story.kids) {
@@ -198,6 +213,7 @@ const getComments = function(storyId) {
                 }));
             });
 
+            // Will get all the necessary stories together, sorted
             Promise.all(commentsPromises).then(function(commentsData) {
                 populateComments(story, commentsData);
             });
@@ -207,6 +223,7 @@ const getComments = function(storyId) {
     });
 }
 
+/* Just a spacer row for display purposes */
 const createSpacerRow = function() {
     const spacerRow = document.createElement('tr');
     spacerRow.className = "spacerRow";
@@ -214,6 +231,7 @@ const createSpacerRow = function() {
     return spacerRow;
 }
 
+/* Returns the span for the story's origin URL */
 const createDomainSpan = function(url) {
     const domain = (new URL(url)).host;
     const domainSpan = document.createElement('span');
@@ -224,8 +242,11 @@ const createDomainSpan = function(url) {
     return domainSpan;
 }
 
+/* Creates the comments content for meta row */
 const createCommentsLink = function(storyUrl, commentsAmount) {
-    const comments = " | " + commentsAmount  + (commentsAmount > 1 ? " comments" : " comment");
+    let comments = " | " + commentsAmount;
+    comments += (commentsAmount > 1 ? " comments" : " comment");
+
     const commentsLink = document.createElement('a');
     commentsLink.href = storyUrl;
     commentsLink.innerHTML += comments;
@@ -233,6 +254,7 @@ const createCommentsLink = function(storyUrl, commentsAmount) {
     return commentsLink;
 }
 
+/* Generates the necessary elements for the stories in the page */
 const populateStories = function(data, startPagination, nextPage) {
     const itemList = document.getElementById('itemList');
     const itemListBody = itemList.getElementsByTagName('tbody')[0];
@@ -329,9 +351,9 @@ const populateStories = function(data, startPagination, nextPage) {
     // Add the More button
     const moreRow = document.createElement('tr');
     const moreRowTopColumn = document.createElement('td');
-    const moreRowTitleColumn = document.createElement('td');
-    const moreLink = document.createElement('a');
     moreRow.appendChild(moreRowTopColumn);
+
+    const moreLink = document.createElement('a');
     moreLink.href = "#" + nextPage;
     moreLink.className = "more";
     moreLink.innerHTML = "More";
@@ -343,6 +365,7 @@ const populateStories = function(data, startPagination, nextPage) {
         getStoriesForCurrentPage(nextPage);
     }
 
+    const moreRowTitleColumn = document.createElement('td');
     moreRowTitleColumn.appendChild(moreLink);
     moreRow.appendChild(moreRowTitleColumn);
 
@@ -352,6 +375,7 @@ const populateStories = function(data, startPagination, nextPage) {
     toggleLoading();
 }
 
+/* Generates the necessary elements for a story and its comments in the page */
 const populateComments = function(story, data) {
     const storyUrl = "/story/" + story.id;
 
@@ -361,6 +385,7 @@ const populateComments = function(story, data) {
     const comments = document.getElementById('comments');
     const commentsBody = comments.getElementsByTagName('tbody')[0];
 
+    // Story row
     const storyRow = document.createElement('tr');
     storyRow.id = story.id;
     storyRow.className = "storyRow";
@@ -387,6 +412,7 @@ const populateComments = function(story, data) {
     storyRow.appendChild(storyRowTitleColumn);
     itemBody.appendChild(storyRow);
 
+    // Story meta row
     storyMetaRow.className = "metaRow";
     storyMetaRowColumn.innerHTML = "";
 
@@ -414,6 +440,7 @@ const populateComments = function(story, data) {
     itemBody.appendChild(storyRow);
     itemBody.appendChild(storyMetaRow);
 
+    // All comments for the story
     data.forEach(function(commentData, position) {
         if(!commentData.deleted && !commentData.dead) {
             const commentDate = new Date(commentData.time*1000);
@@ -439,6 +466,7 @@ const populateComments = function(story, data) {
     });
 }
 
+// init()
 document.addEventListener("DOMContentLoaded", function(event) {
     if(isStoryView()) {
         const storyId = getStoryIdFromUrl();
